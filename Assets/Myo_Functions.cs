b3b9954d89bdddf3a1ca99188c231a8179d412a9
harmon758 <harmon758@gmail.com>
2014-12-11 14:32:41 -0600

    TheForce_5

---
+++ Assets/Myo_Functions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Myo_Functions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Myo game object to connect with.
+	// This object must have a ThalmicMyo script attached.
+	public GameObject myo = null;
+	
+	// Update is called once per frame
+	void Update () {
+		ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo> ();
+		if(thalmicMyo.pose == Thalmic.Myo.Pose.Fist){
+			print("Test");
+		}
+		rigidbody.AddForce(19 * thalmicMyo.accelerometer.x * rigidbody.mass, 19 * thalmicMyo.accelerometer.z * rigidbody.mass, 19 * thalmicMyo.accelerometer.y * rigidbody.mass);
+	}
+}
